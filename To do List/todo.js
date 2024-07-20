79a424db60d58cadd5fc35a6a446e822344f0984
MRK501 <manukhedekar501@gmail.com>
2024-07-20 18:26:02 +0530

    4th commit

---
+++ To do List/todo.js
@@ -0,0 +1,33 @@
+let todoList = [
+  { item: 'Code', dueDate: '15/7/2024' },
+  { item:'Solve', dueDate: '15/7/2024' },
+  { item: 'Create', dueDate: '15/7/2024' }];
+  
+displayItems();
+
+function addTodo() {
+  let inputElement = document.querySelector('#todo-input');
+  let dateElement = document.querySelector('#todo-date');
+  let todoItem = inputElement.value;
+  let todoDate = dateElement.value;
+  todoList.push({ item:todoItem,dueDate:todoDate });
+  inputElement.value = ' ';
+  dateElement.value = ' ';
+  displayItems();
+}
+function displayItems() {
+  let containerElement = document.querySelector('.todo-container');
+  let newHtml = '';
+  for (i = 0; i < todoList.length; i++){
+
+    let { item, dueDate } = todoList[i];
+    newHtml += `
+    <span>${item}</span>
+    <span>${dueDate}</span>
+    <button class="btn-delete"onclick="todoList.splice(${i},1);
+    displayItems();">Delete</button>
+    `;
+  }
+  containerElement.innerHTML = newHtml;
+  
+}
